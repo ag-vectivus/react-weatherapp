7a16fd83aa0aa669f56aadef3b6b1e24f0e35794
ag-vectivus <a.s.galecki@gmail.com>
2021-05-08 14:12:56 +0200

    Reorganize file structure.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import WeatherContextProvider from "./contexts/WeatherContext";
+import LocationForm from "../src/components/LocationForm";
+import Forecast from "../src/components/Forecast";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='container my-5 mx-auto'>
+			<h1 className='text-center text-muted my4'>Weather App</h1>
+			<WeatherContextProvider>
+				<LocationForm />
+				<Forecast />
+			</WeatherContextProvider>
+		</div>
+	);
 }
 
 export default App;
